66155ee406394872fdaa0db3c35e8b13ef6379e9
José Edson <joseedsontg@gmail.com>
2025-10-06 17:37:17 -0300

    Aula número 4

---
+++ Aula4.py
@@ -0,0 +1,26 @@
+#Seção 1: Lógica de Programação
+#Essa aula é sobre o tipo de dados Strings
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("Luiz \"Otávio\"")#desse jeito consegue printar as áspas também
+
+# r
+print(r"Luiz \"Otávio\"")#desse jeito se printa tudo o que colocar dentro das áspas principais do print
