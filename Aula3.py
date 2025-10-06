97f6de68f2a5c3ea5a3a774c28d9a1139530ec03
José Edson <joseedsontg@gmail.com>
2025-10-06 17:20:46 -0300

    Aula número 3

---
+++ Aula3.py
@@ -0,0 +1,5 @@
+#Seção 1: Lógica de Programação
+#Nessa aula será descrito um macete para a função print
+
+print(12, 34, sep ="-")#sep serve para escolher um separador quando você quiser mostrar números seprados
+print(56, 78, sep ="-")#então você coloca algo para poder ficar no lugar do espaço que ficaria normalmente
