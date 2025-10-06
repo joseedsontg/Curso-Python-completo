630950584882764de47af2be3a3fc4d3e4a90a03
José Edson <joseedsontg@gmail.com>
2025-10-06 17:07:23 -0300

    Aula número 2

---
+++ Aula2.py
@@ -0,0 +1,8 @@
+#Seção 1: Lógica de Programação
+#Essa aula introduz o Docstrings
+
+"""
+Docstrigs é quando juntamos 3 aspas duplas( " ) ou simples( ' ) para poder fazer comentários que peguem 
+mais deuma linha, já que o comentário oficial do python que é feito com o 
+hashtag ou jogo da velha ( # ) só pega o que vem após dele e não passa para outra linha
+"""
