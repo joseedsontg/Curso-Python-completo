8ed87c7514497fe0a7ac882cdaad4f7dd24cc0fb
José Edson <joseedsontg@gmail.com>
2025-10-06 16:17:37 -0300

    Commit de teste e explicação

--- teste.py
+++ teste.py
@@ -1,1 +1,5 @@
 print("Hello World!!!")
+#A partir deste ponto irei deixar escrito antes de cada código a seção do curso a qual eles pertencem
+#por exemplo: #Seção 1: Lógica de Programação
+#e então deixarei uma explicação do código, dizendo qual seu propósito
+#também farei comentários nos códigos explicando partes que eu ache importantes
