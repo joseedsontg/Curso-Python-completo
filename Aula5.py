abad662fb4df33992a4b1e126ff715d88f8e6dc3
José Edson <joseedsontg@gmail.com>
2025-10-06 18:58:46 -0300

    Aula número 5

---
+++ Aula5.py
@@ -0,0 +1,24 @@
+#Seção 1: Lógica de Programação
+#Essa aula é sobre os tipos de dados Inteiros(int) e Ponto Flutuante(float)
+
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado
+# positivo.
+# print(11) # int
+# print(-11) # int
+# print(0)
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+# print(1.1, 10.11)
+# print(0.0, -1.5)
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor.
+
+print(10, -10) #exemplo de int
+print(1.45, 4.37) #exemplo de float
