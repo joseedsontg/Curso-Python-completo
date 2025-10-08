4431e73faa8b1645de3b220d71cf2442ed721d3b
José Edson <joseedsontg@gmail.com>
2025-10-08 16:01:46 -0300

    Aula número 7

---
+++ Aula7.py
@@ -0,0 +1,13 @@
+#Seção 1: Lógica de programação
+#Essa aula é sobre coerção de dados 
+#que é converter um dado de um tipo para outro
+
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
