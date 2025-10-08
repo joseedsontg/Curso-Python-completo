193a8cc4f77eedf60086089ff36bb4a5bc10b372
José Edson <joseedsontg@gmail.com>
2025-10-08 16:30:47 -0300

    Exercício da aula 8

---
+++ Aula8_Exercicio.py
@@ -0,0 +1,16 @@
+#Seção 1: Lógica de Programação
+#Exercício simples:
+#printar seu nome, idade, ano de nascimento, altura em metros
+#depois printar se você é maior de idade
+
+nome = 'Jose'
+idade = 19
+ano_de_nascimento = 2006
+altura_em_metros = 1.82
+maior_de_idade = idade >= 18
+
+print("Nome :", nome)
+print("Idade :", idade)
+print("Ano de nascimento :", ano_de_nascimento)
+print("Altura em metros :", altura_em_metros)
+print("É maior?", maior_de_idade)
