2b99d845cd92efb293bd60c0cd76b8599620871a
José Edson <joseedsontg@gmail.com>
2025-10-08 15:50:59 -0300

    Aula número 6

---
+++ Aula6.py
@@ -0,0 +1,21 @@
+#Seção 1: Lógica de Programação
+#Essa aula é sobre o tipo de dado boleano(bool)
+
+'''
+Um bool é um valor lógico que vai ser atribuido a algo
+como TRUE ou FALSE
+'''
+
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que
+# questiona se um valor é igual a outro
+print(10 == 10)  # Sim => True (Verdadeiro)
+print(10 == 11)  # Não => False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
