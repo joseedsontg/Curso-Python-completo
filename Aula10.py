4c50776e2f2797f59679929cb834a4726cd00343
José Edson <joseedsontg@gmail.com>
2025-10-20 18:20:34 -0300

    Aula número 10

---
+++ Aula10.py
@@ -0,0 +1,12 @@
+#Seção 1: Lógica de Programação
+"""Essa aula é sobre concatenação(+) e repetição(*) com 
+   operadores aritmétcos 
+"""
+
+concatenacao = 'A' + 'B' + 'C' #concatena(junta) strings
+print(concatenacao) #a saída é ABC
+
+a_dez_vezes = 'A' * 10
+tres_vezes_jose = 3 * 'Jose' # a ordem que você coloca não importa
+print(a_dez_vezes) # vai printar A dez vezes seguidas sem espaço
+print(tres_vezes_jose) # vai printar Jose três vezes seguidas sem espaço
