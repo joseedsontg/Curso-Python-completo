c5ebb21fa4f351d5b47d42973eaf38e29d9be7cf
José Edson <joseedsontg@gmail.com>
2025-11-15 11:07:31 -0300

    Aula número 12

---
+++ Aula12.py
@@ -0,0 +1,22 @@
+#Seção 1: Lógica de Programação
+#Essa aula é sobre f-strings (formatação de strings), uma introdução breve
+
+#usa-se como base o print que eu usei no exercício da aula 11
+nome = 'Jose Edson'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'#coloca um f no começo da string
+linha_2 = f'pesa {peso} quilos e seu imc é'#para usar variáveis dentro da string temos que colocar entra chaves {}
+linha_3 = f'{imc:.2f}'#para formatar números usamos : e depois o formato desejado
+#no caso .2f formata o número para float com 2 casas decimais
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Jose Edson tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
