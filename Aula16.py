06d5b6fcd723e336d4574a2a5f9c8821c927ba94
José Edson <joseedsontg@gmail.com>
2025-12-03 19:55:37 -0300

    Aula número 16

---
+++ Aula16.py
@@ -0,0 +1,22 @@
+#Seção 1: Lógica de Programação
+#Esta aula é uma introdução aos operadores relacionais em Python
+#Operadores relacionais são usados para comparar valores e retornar um valor booleano (True ou False)
+#Vamos explorar os principais operadores relacionais: ==, !=, >, <, >=,
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('Olha meu print aqui')
