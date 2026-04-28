aa95191362b575063deb5cd9883c73395469b1fd
José Edson <joseedsontg@gmail.com>
2026-04-28 14:14:56 -0300

    Aula número 24

--- Aula24.py
+++ Aula24.py
@@ -17,7 +17,7 @@ try:
 except:
     print('Isso não é um número')
 
-# if numero_str.isdigit():
+# if numero_str.isdigit(): //isdigit() verifica se o usuário digitou somente números
 #     numero_float = float(numero_str)
 #     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 # else:
