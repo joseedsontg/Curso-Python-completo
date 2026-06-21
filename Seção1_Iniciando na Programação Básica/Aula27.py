7c258be36b69e9e62337233a352950320279874e
José Edson <joseedsontg@gmail.com>
2026-06-21 17:43:30 -0300

    Aula número 27

---
+++ Seção1_Iniciando na Programação Básica/Aula27.py
@@ -0,0 +1,15 @@
+#Seção 1
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Tipos imutáveis: str, int, float, bool
+"""
+
+string = 'José Edson'
+inteiro = int(10)
+flutuante = float(1.0)
+booleano = bool(True)
+
+print(string)
+print(inteiro)
+print(flutuante)
+print(booleano)
